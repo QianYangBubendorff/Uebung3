48bd8bca502f418521c46560747a30e3fc7ee9b5
QianYangBubendorff <qian.yangbubendorff@gmail.com>
2022-03-10 15:33:19 +0100

    Initial commit

---
+++ src/ZinsDemo.java
@@ -0,0 +1,8 @@
+public class ZinsDemo {
+    public static void main(String[] args) {
+        System.out.println (zinsenValue(1000.0, 2.25));
+    }
+    public static double zinsenValue (double kontostand, double zinsen){
+        return kontostand * zinsen;
+    }
+}
