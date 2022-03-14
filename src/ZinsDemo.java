f9f6819f79982efa1395c78e80760fcbd0ee3310
QianYangBubendorff <qian.yangbubendorff@gmail.com>
2022-03-14 14:07:50 +0100

    changes

--- src/ZinsDemo.java
+++ src/ZinsDemo.java
@@ -2,7 +2,7 @@ public class ZinsDemo {
     public static void main(String[] args) {
         System.out.println (zinsenValue(1000.0, 2.25));
     }
-    public static double zinsenValue (double kontostand, double zinsen){
-        return kontostand * zinsen;
+    public static double zinsenValue (double kontostand, double zinsen) {
+        return kontostand * zinsen;}
     }
 }
