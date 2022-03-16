cbfead182c8f7f0d85526713c52dd1d2708e456e
QianYangBubendorff <qian.yangbubendorff@gmail.com>
2022-03-16 15:29:36 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/vcs.xml
    #	src/Grossbuchstaben.java

--- src/NumberHelper.java
+++ src/NumberHelper.java
@@ -2,8 +2,9 @@ import java.util.Arrays;
 
 public class NumberHelper {
     public static void main(String[] args) {
-        int[][] arr3 = new int[][]{{0, 3, 4, 10}, {3, 4, 5}};
-        System.out.println(Arrays.toString(getFlatArray(arr3)));
+        int[][] arr = new int[][]{{0, 3, 4, 10}, {3, 4, 5}};
+        System.out.println(Arrays.toString(getFlatArray(arr)));
+        System.out.println(Arrays.toString(removeDuplicate(arr)));
 
     }
 
@@ -41,29 +42,31 @@ public class NumberHelper {
             }
         }
 
-        // Creating new array which combine the number of the 2D array with the length l.
         int[] arr2 = new int[l];
         int k = 0;
         for (int i = 0; i < numbers.length; i++) {
             for (int j = 0; j < numbers[i].length; j++) {
-                if (numbers[i][j] != numbers[i][j + 1]) {
-                    arr2[k] = numbers[i][j];
-                    k++;
-                }
+                arr2[k] = numbers[i][j];
+                k++;
             }
         }
-        int[] arr3= new int[l];
-        int temp=-1;
-        for (int i=0; i<arr2.length; i++){
-            for(int j=1; j<arr2.length; j++){
-            if(arr2[i]!=arr2[j]{
-                    arr3[k]=arr2[i];
-                    k++;
-            } else{
-                arr3[k]
-                }
+        // Creating new array which combine the number of the 2D array with the length l.
+        int[] arr3 = new int[l];
+        for (int i = 0; i < l; l++) {
+            arr3[i] = -1;
+        }
+        for (int i = 0; i < arr2.length; i++) {
+            for (int j = i + 1; j < arr2.length; j++) {
+                if (arr2[i] == arr2[j]) {
+                    arr3[j] = -1;
+
+                }else arr3[i]= arr2[i];
+
+
+            }
+
         }
         return arr3;
     }
-
 }
+
