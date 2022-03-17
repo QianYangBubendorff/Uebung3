f8f6f3fb4b27a1addf400b79ec68e06f2831c1d0
QianYangBubendorff <qian.yangbubendorff@gmail.com>
2022-03-17 15:48:27 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/vcs.xml
    #	src/Grossbuchstaben.java

--- src/Primefactor.java
+++ src/Primefactor.java
@@ -1,29 +1,31 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Primefactor {
     public static void main(String[] args) {
-        System.out.println(findBiggestPf(50));
+        System.out.println(findBiggestPrimeFactor(13195));
+        }
+
+    public static int findBiggestPrimeFactor(int number){
+        List<Integer> list1 = new ArrayList<Integer>();
+        for (int i = 1; i <= number; i++) {
+        if (number % i == 0) {
+            list1.add(i);
+        }
     }
-
-    public static int findBiggestPf(int number) {
-        int a = 0;
-        int[] list = new int[a];
-        for (int i = 1; i <= number / 2; i++) {
-            if (number % i == 0) {
-               list[a]=i;
-                a++;
+        int biggestfactor=0;
+        for(int i= list1.size()-2; i>=0; i--) {
+        for(int j=2; j<=list1.get(i)/2; j++){
+            if(list1.get(i)% j!=0 ){
+                biggestfactor=list1.get(i);
+                return biggestfactor;
             }
         }
-        int temp = 0;
-        for (int i = 0; i < list.length; i++) {
-            temp = list[i];
-            if (list[i] < list[i + 1]) {
-                list[i + 1] = list[i];
-                list[i + 1] = temp;
-            }
         }
-        return temp;
-
-
+        return biggestfactor;
     }
 }
+
 
 
